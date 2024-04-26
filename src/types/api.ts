15890fa98d696355bb5384f6ea362d4511301629
Gouseppe <fernandeitorc@gmail.com>
2024-04-26 15:46:37 -0400

    feat: add pokemons types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,3 +1,15 @@
+export type APIPokemons = {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+};
+
+export type Result = {
+  name: string;
+  url: string;
+};
+
 export type APIPokemon = {
   abilities: Ability[];
   base_experience: number;
