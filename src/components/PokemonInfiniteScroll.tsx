21371a4ed575e992f4f48e169b6a97653bf6e1fa
Gouseppe <fernandeitorc@gmail.com>
2024-04-30 20:43:32 -0400

    feat: upgrade the PokemonInfiniteScroll

--- src/components/PokemonInfiniteScroll.tsx
+++ src/components/PokemonInfiniteScroll.tsx
@@ -3,44 +3,60 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import type { APIPokemon, PokemonTypes } from '../types';
 import { ReactCard } from './ReactCard';
 import { getPokemons } from '../config/api/backend/pokemons';
+import { $filter, $pokemons, setFilter, setPokemon } from '../shared';
+import { useStore } from '@nanostores/react';
+import { getPokemon } from '../config/api/backend/pokemon';
 
 export const PokemonInfiniteScroll = () => {
-  const [pokemons, setPokemons] = useState<APIPokemon[]>([]);
-
-  const [filter, setFilter] = useState<{ cant: number; page: number }>({
-    cant: 30,
-    page: 0,
-  });
+  const pokemons = useStore($pokemons);
+  const filter = useStore($filter);
 
   useEffect(() => {
     (async () => {
-      const pokemonsUrl = await getPokemons(
-        filter.cant,
-        filter.page * filter.cant
-      );
-      const pokemosData: APIPokemon[] = await Promise.all(
-        pokemonsUrl.map(async (pokemon: { url: RequestInfo | URL }) => {
-          const response = await fetch(pokemon.url);
-          const data = await response.json();
-          return data;
-        })
-      );
-      setPokemons([...pokemons, ...pokemosData]);
+      if (pokemons.length !== filter.cant * (filter.page + 1)) {
+        console.log('hizo el fetch');
+        const pokemonsUrl = await getPokemons(
+          filter.cant,
+          filter.page * filter.cant
+        );
+        const pokemosData: APIPokemon[] = await Promise.all(
+          pokemonsUrl.map(async (pokemon: { name: string }) => {
+            const response = await getPokemon(pokemon.name);
+            return response as APIPokemon;
+          })
+        );
+        pokemons.length === 1
+          ? setPokemon([...pokemosData])
+          : setPokemon([...pokemons, ...pokemosData]);
+      }
     })();
   }, [filter.cant, filter.page]);
 
-  const fetchMoreData = () => {
+  const updateFilter = () => {
     setFilter({ cant: 30, page: filter.page + 1 });
-    console.log('acctualiza');
   };
   return (
     <div>
-      <InfiniteScroll
-        dataLength={pokemons.length || 0}
-        next={fetchMoreData}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-      >
+      {pokemons.length > 1 ? (
+        <InfiniteScroll
+          dataLength={pokemons.length || 0}
+          next={updateFilter}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className='gap-4 grid grid-cols-link-card justify-items-center'>
+            {pokemons?.map((pokemon) => (
+              <ReactCard
+                key={pokemon.id}
+                id={pokemon.id}
+                image={pokemon.sprites.other['official-artwork'].front_default}
+                title={pokemon.name}
+                types={pokemon.types.map((type: any) => type.type.name)}
+              />
+            ))}
+          </div>
+        </InfiniteScroll>
+      ) : (
         <div className='gap-4 grid grid-cols-link-card justify-items-center'>
           {pokemons.length > 0 &&
             pokemons?.map((pokemon) => (
@@ -52,20 +68,9 @@ export const PokemonInfiniteScroll = () => {
                 types={pokemon.types.map((type: any) => type.type.name)}
               />
             ))}
+          {pokemons.length === 0 && <p>No hay pokemones</p>}
         </div>
-      </InfiniteScroll>
+      )}
     </div>
   );
 };
-
-//   fetchMoreData = () => {
-//     // a fake async api call like which sends
-//     // 20 more records in 1.5 secs
-//     setTimeout(() => {
-//       this.setState({
-//         items: this.state.items.concat(Array.from({ length: 20 }))
-//       });
-//     }, 1500);
-//   };
-
-// }
