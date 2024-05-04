ae43336e3c506bc3b6e82da6d69ca90165dc5713
Gouseppe <fernandeitorc@gmail.com>
2024-05-04 11:01:58 -0400

    feat: upgrade ThemeToggler

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,12 @@ export default {
       gridTemplateColumns: {
         'link-card': 'repeat(auto-fit, minmax(24ch, 1fr))',
       },
+      boxShadow: {
+        'border-toggle-dark':
+          'inset 0 0 10px rgb(76 29 149), 0 0 10px rgb(76 29 149)',
+        'border-toggle-light':
+          'inset 0 0 10px rgb(254 240 138), 0 0 10px rgb(254 240 138)',
+      },
     },
   },
   plugins: [],
