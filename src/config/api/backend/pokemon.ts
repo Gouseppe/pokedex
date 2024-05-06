2e78c2cbf0f40248e013f600248e56a425d734ca
Gouseppe <fernandeitorc@gmail.com>
2024-05-05 23:23:04 -0400

    fix: eliminate case sensitive in the api querys

--- src/config/api/backend/pokemon.ts
+++ src/config/api/backend/pokemon.ts
@@ -4,7 +4,7 @@ import { API_URL } from '../../consts';
 export const getPokemon = async (
   name: string
 ): Promise<APIPokemon | undefined> => {
-  const response = await fetch(`${API_URL}pokemon/${name}`);
+  const response = await fetch(`${API_URL}pokemon/${name.toLowerCase()}`);
   if (response.status === 200) {
     const pokemon = await response.json();
     return pokemon;
