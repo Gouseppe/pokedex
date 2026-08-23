1c4d2c4d4dd63d4b83a7ed9ebd6e8042721e0194
Gouseppe <fernandeitorc@gmail.com>
2026-08-23 18:44:33 -0400

    refactor: update $pokemons type to APIPokemon and clean up commented code

--- src/shared/pokemons.ts
+++ src/shared/pokemons.ts
@@ -1,12 +1,29 @@
+// import { atom } from "nanostores";
+// import type { APIPokemon } from "../types";
+// import type { PokemonDeno } from "../types/api/deno-api";
+
+// export const $pokemons = atom<PokemonDeno[] | undefined>([]);
+// export const $pokemonName = atom<string>("");
+// export const $pokemon = atom<APIPokemon | undefined | null>(undefined);
+
+// export const setPokemons = (pokemons: PokemonDeno[] | undefined) => {
+//   $pokemons.set(pokemons);
+// };
+// export const setPokemon = (pokemon: APIPokemon | undefined | null) => {
+//   $pokemon.set(pokemon);
+// };
+
+// export const setPokemonName = (name: string) => {
+//   $pokemonName.set(name);
+// };
 import { atom } from "nanostores";
 import type { APIPokemon } from "../types";
-import type { PokemonDeno } from "../types/api/deno-api";
 
-export const $pokemons = atom<PokemonDeno[] | undefined>([]);
+export const $pokemons = atom<APIPokemon[] | undefined>([]);
 export const $pokemonName = atom<string>("");
 export const $pokemon = atom<APIPokemon | undefined | null>(undefined);
 
-export const setPokemons = (pokemons: PokemonDeno[] | undefined) => {
+export const setPokemons = (pokemons: APIPokemon[] | undefined) => {
   $pokemons.set(pokemons);
 };
 export const setPokemon = (pokemon: APIPokemon | undefined | null) => {
